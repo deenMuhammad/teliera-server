59174e585f922cf9a988feb6989818c4770e2975
Gyeong-Sik Park <tpnet3@gmail.com>
2018-12-07 18:06:12 +0900

    wip schema

--- resolvers/Mutation/enroll.js
+++ resolvers/Mutation/enroll.js
@@ -1,7 +1,7 @@
 const fetch = require('node-fetch')
 
-const enroll = (obj, { id }, { headers }) => {
-    return fetch(`http://courses.node.internal.getbouncecode.com:3000/course/${id}/enroll`, { method: 'POST', headers })
+const enroll = (obj, { courseId }, { headers }) => {
+    return fetch(`http://courses.node.internal.getbouncecode.com:3000/course/${courseId}/enroll`, { method: 'POST', headers })
         .then(data => data.json())
         .then(json => json.data);
 }
--- resolvers/Query/section.js
+++ resolvers/Query/section.js
@@ -1,7 +1,7 @@
 const fetch = require('node-fetch')
 
-const section = (obj, { id }, { headers }) => {
-    return fetch(`http://courses.node.internal.getbouncecode.com:3000/section/${id}`, { method: 'GET', headers })
+const section = (obj, { sectionId }, { headers }) => {
+    return fetch(`http://courses.node.internal.getbouncecode.com:3000/section/${sectionId}`, { method: 'GET', headers })
         .then(data => data.json())
         .then(json => json.data);
 }

26361aeeab82ac83639a7badfa3ee946b9756547
Gyeong-Sik Park <tpnet3@gmail.com>
2018-12-08 17:07:32 +0900

    bugfix

--- resolvers/Mutation/node-instance.js
+++ resolvers/Mutation/node-instance.js
@@ -1,85 +1,85 @@
 const fetch = require('node-fetch')
 
 const startContainer = (obj, { image, env, git }, { headers }) => {
-    return fetch(`http://instance.node.internal.getbouncecode.com:3000/container`, {
-        method: 'POST',
-        headers
-    })
-    .then(data => data.json());
+  return fetch(`http://instance.node.internal.getbouncecode.com:3000/container`, {
+    method: 'POST',
+    headers
+  })
+  .then(data => data.json());
 }
 
 const stopContainer = (obj, { containerId }, { headers }) => {
-    return fetch(`http://instance.node.internal.getbouncecode.com:3000/container/${containerId}`, {
-        method: 'DELETE',
-        headers
-    })
-    .then(data => data.json())
-    .then(json => !!json.success);
+  return fetch(`http://instance.node.internal.getbouncecode.com:3000/container/${containerId}`, {
+    method: 'DELETE',
+    headers
+  })
+  .then(data => data.json())
+  .then(json => !!json.success);
 }
 
 const commitContainer = (obj, { containerId }, { headers }) => {
-    return fetch(`http://instance.node.internal.getbouncecode.com:3000/container/${containerId}/commit`, {
-        method: 'POST',
-        headers
-    })
-    .then(data => data.json())
-    .then(json => !!json.success);
+  return fetch(`http://instance.node.internal.getbouncecode.com:3000/container/${containerId}/commit`, {
+    method: 'POST',
+    headers
+  })
+  .then(data => data.json())
+  .then(json => !!json.success);
 }
 
 const testContainer = (obj, { containerId, workgit, testgit, image, cmd, env }, { headers }) => {
-    return fetch(`http://instance.node.internal.getbouncecode.com:3000/container/${containerId}/test`, {
-        method: 'POST',
-        headers,
-        body: JSON.stringify({
-            workgit,
-            testgit,
-            image,
-            cmd,
-            env
-        })
+  return fetch(`http://instance.node.internal.getbouncecode.com:3000/container/${containerId}/test`, {
+    method: 'POST',
+    headers,
+    body: JSON.stringify({
+      workgit,
+      testgit,
+      image,
+      cmd,
+      env
     })
-    .then(data => data.json())
-    .then(json => ({...json.insp, Output: json.data}));
+  })
+  .then(data => data.json())
+  .then(json => ({...json.insp, Output: json.data}));
 }
 
 const runExec = (obj, { containerId, cmd }, { headers }) => {
-    return fetch(`http://instance.node.internal.getbouncecode.com:3000/container/${containerId}/exec`, {
-        method: 'POST',
-        headers,
-        body: JSON.stringify({
-            cmd
-        })
+  return fetch(`http://instance.node.internal.getbouncecode.com:3000/container/${containerId}/exec`, {
+    method: 'POST',
+    headers,
+    body: JSON.stringify({
+      cmd
     })
-    .then(data => data.json());
+  })
+  .then(data => data.json());
 }
 
 const waitExec = (obj, { containerId, cmd }, { headers }) => {
-    return fetch(`http://instance.node.internal.getbouncecode.com:3000/container/${containerId}/exec_wait`, {
-        method: 'POST',
-        headers,
-        body: JSON.stringify({
-            cmd
-        })
+  return fetch(`http://instance.node.internal.getbouncecode.com:3000/container/${containerId}/exec_wait`, {
+    method: 'POST',
+    headers,
+    body: JSON.stringify({
+      cmd
     })
-    .then(data => data.json())
-    .then(json => ({...json.insp, Output: json.data}));
+  })
+  .then(data => data.json())
+  .then(json => ({...json.insp, Output: json.data}));
 }
 
 const killExec = (obj, { execId }, { headers }) => {
-    return fetch(`http://instance.node.internal.getbouncecode.com:3000/exec/${execId}`, {
-        method: 'DELETE',
-        headers
-    })
-    .then(data => data.json())
-    .then(json => !!json.success);
+  return fetch(`http://instance.node.internal.getbouncecode.com:3000/exec/${execId}`, {
+    method: 'DELETE',
+    headers
+  })
+  .then(data => data.json())
+  .then(json => !!json.success);
 }
 
 module.exports = {
-    startContainer,
-    stopContainer,
-    commitContainer,
-    testContainer,
-    runExec,
-    killExec,
-    waitExec
+  startContainer,
+  stopContainer,
+  commitContainer,
+  testContainer,
+  runExec,
+  killExec,
+  waitExec
 }
--- resolvers/Query/node-courses.js
+++ resolvers/Query/node-courses.js
@@ -1,27 +1,39 @@
 const fetch = require('node-fetch')
 
 const course = (obj, { courseId }, { headers }) => {
-    return fetch(`http://courses.node.internal.getbouncecode.com:3000/course/${courseId}`, { method: 'GET', headers })
-        .then(data => data.json())
-        .then(json => json.data);
+  return fetch(`http://courses.node.internal.getbouncecode.com:3000/course/${courseId}`, {
+    method: 'GET',
+    headers
+  })
+  .then(data => data.json())
+  .then(json => json.data);
 }
 
 const courses = (obj, { enrolled }, { headers }) => {
   if (enrolled) {
-      return fetch('http://courses.node.internal.getbouncecode.com:3000/enrolled', { method: 'GET', headers })
-          .then(data => data.json())
-          .then(json => json.data);
+    return fetch('http://courses.node.internal.getbouncecode.com:3000/enrolled', {
+      method: 'GET',
+      headers
+    })
+    .then(data => data.json())
+    .then(json => json.data);
   } else {
-      return fetch('http://courses.node.internal.getbouncecode.com:3000/course', { method: 'GET', headers })
-          .then(data => data.json())
-          .then(json => json.data);
+    return fetch('http://courses.node.internal.getbouncecode.com:3000/course', {
+      method: 'GET',
+      headers
+    })
+    .then(data => data.json())
+    .then(json => json.data);
   }
 }
 
 const section = (obj, { sectionId }, { headers }) => {
-  return fetch(`http://courses.node.internal.getbouncecode.com:3000/section/${sectionId}`, { method: 'GET', headers })
-      .then(data => data.json())
-      .then(json => json.data);
+  return fetch(`http://courses.node.internal.getbouncecode.com:3000/section/${sectionId}`, {
+    method: 'GET',
+    headers
+  })
+  .then(data => data.json())
+  .then(json => json.data);
 }
 
 module.exports = {

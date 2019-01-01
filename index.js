7887857747b45669d90562b4ef3293b5f431bc10
Gyeong-Sik Park <tpnet3@gmail.com>
2019-01-01 13:27:56 +0900

    update cors

--- index.js
+++ index.js
@@ -1,27 +1,36 @@
-const { ApolloServer, gql } = require('apollo-server');
+const fs = require('fs');
+const express = require('express');
+const { ApolloServer, gql } = require('apollo-server-express');
+const cors = require('cors')
+
+const typeDefs = gql(fs.readFileSync(__dirname + '/schema.graphql').toString());
+
 const resolvers = require('./resolvers');
-const fs = require('fs')
 
-const server = new ApolloServer({
-  typeDefs: gql(fs.readFileSync(__dirname + '/schema.graphql').toString()),
-  resolvers,
-  context: ({ req }) => {
-    const userAgent = req.headers['user-agent'];
-    const accountId = req.headers['x-accounts-id'];
-    const cookie = req.headers['cookie'];
+const context = ({ req }) => {
+  const userAgent = req.headers['user-agent'];
+  const accountId = req.headers['x-accounts-id'];
+  const cookie = req.headers['cookie'];
 
-    return {
-      req,
-      headers: {
-        'User-Agent': userAgent || '',
-        'X-Accounts-Id': accountId || '',
-        'Cookie': cookie || '',
-        'Content-Type': 'application/json'
-      }
+  return {
+    req,
+    headers: {
+      'User-Agent': userAgent || '',
+      'X-Accounts-Id': accountId || '',
+      'Cookie': cookie || '',
+      'Content-Type': 'application/json'
     }
-  },
-});
+  }
+};
 
-server.listen(3000).then(({ url }) => {
-  console.log(`🚀 Server ready at ${url}`);
-});
+const server = new ApolloServer({ typeDefs, resolvers, context });
+
+const app = express();
+server.applyMiddleware({ app });
+app.use(cors({credentials: true}));
+
+const port = 4000;
+
+app.listen({ port }, () =>
+  console.log(`🚀 Server ready at http://localhost:${port}${server.graphqlPath}`),
+);

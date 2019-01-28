1e22190d00a1c923bb8ea8c14378cd78e74e68df
Dinmukhammad Rakhimov <dinmukhammadrakhimov@Gyeongsiks-MacBook-Pro.local>
2019-01-28 19:11:21 +0900

    make it better

--- db/setup/index.js
+++ db/setup/index.js
@@ -6,7 +6,7 @@ const Order = require('./../models/order');
 const Cart = require('./../models/cart');
 const Liked = require('./../models/liked');
 
-var dbURI = "mongodb://deenmuhammad:+1587455Zorro@ds033255.mlab.com:33255/teliera"
+var dbURI = "mongodb://<deenmuhammad>:<+1587455Zorro>@ds033255.mlab.com:33255/teliera"
 mongoose.connect(dbURI, { useNewUrlParser: true })
 mongoose.connection.on('connected', ()=>{
     console.log("Connected to Database");

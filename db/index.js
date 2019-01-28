44112c81763fbb8c2615f24738ebbf47f015e632
Dinmukhammad Rakhimov <dinmukhammadrakhimov@Gyeongsiks-MacBook-Pro.local>
2019-01-28 19:24:30 +0900

    make it better

--- db/index.js
+++ db/index.js
@@ -1,14 +1,14 @@
 const mongoose = require('mongoose');
 const Product = require('./models/product');
 
-var dbURI = "mongodb://<deenmuhammad>:<+1587455Zorro>@ds033255.mlab.com:33255/teliera";
+var dbURI = "mongodb://deenmuhammad:+1587455Zorro@ds033255.mlab.com:33255/teliera";
 mongoose.connect(dbURI, { useNewUrlParser: true })
 mongoose.connection.on('connected', ()=>{
     console.log("Connected to Database");
 });
-mongoose.connection.on('error', ()=>{
-    throw new Error("Failed to Connect to Databse");
+mongoose.connection.on('error', (err)=>{
+    throw new Error("Failed to Connect to Database "+err);
 });
-mongoose.connection.on('disconnected', ()=>{
-    throw new Error("Databse Disconnected");
+mongoose.connection.on('disconnected', (err)=>{
+    throw new Error("Database Disconnected "+err);
 });

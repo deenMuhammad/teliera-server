c098956e8430b301780edb1381f3612067942c0e
Dinmukhammad Rakhimov <dinmukhammadrakhimov@Gyeongsiks-MacBook-Pro.local>
2019-01-29 15:39:51 +0900

    making better

--- index.js
+++ index.js
@@ -1,8 +1,9 @@
 const fs = require('fs');
-const express = require('express');
-const { ApolloServer, gql } = require('apollo-server-express');
-const cors = require('cors')
- 
+// const express = require('express');
+// const { ApolloServer, gql } = require('apollo-server-express');
+// const cors = require('cors')
+const { ApolloServer, gql } = require('apollo-server'); 
+
 require('./db/index')
 
 const typeDefs = gql(fs.readFileSync(__dirname + '/schema.graphql').toString());
@@ -21,11 +22,46 @@ const context = ({ req }) => {
 const server = new ApolloServer({ typeDefs, resolvers, context });   //this line is used when we need headers
 // const server = new ApolloServer({ typeDefs, resolvers });
 
-const app = express();
-server.applyMiddleware({ app, path: 'graphql' });
-app.use(cors({credentials: true}));
-const port = process.env.PORT;
+// const app = express();
 
-app.listen({port: port}, () =>
-  console.log(`🚀 Server ready at https://teliera.herokuapp.com:${port}/graphql`),
-);
+server.listen({ port: process.env.PORT || 4000 }).then(({ url }) => {
+  console.log(`🚀 Server ready at ${url}`);
+});
+// app.use(cors({credentials: true}));
+// const port = process.env.PORT;
+
+// app.listen({port: port}, () =>
+//   console.log(`🚀 Server ready at https://teliera.herokuapp.com:${port}/graphql`),
+// );
+
+
+// const { ApolloServer, gql } = require('apollo-server');
+
+// // The GraphQL schema
+// const typeDefs = gql`
+//   type Query {
+//     hello: String
+//     mockedString: String
+//   }
+// `;
+
+// // A map of functions which return data for the schema.
+// const resolvers = {
+//   Query: {
+//     hello: () =>
+//       fetch('https://fourtonfish.com/hellosalut/?mode=auto')
+//         .then(res => res.json())
+//         .then(data => data.hello),
+//   },
+// };
+
+// const server = new ApolloServer({
+//   typeDefs,
+//   resolvers,
+//   mocks: true,
+//   onHealthCheck: () => fetch('https://fourtonfish.com/hellosalut/?mode=auto'),
+// });
+
+// server.listen().then(({ url }) => {
+//   console.log(`🚀 Server ready at ${url}`);
+// });

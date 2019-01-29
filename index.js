f0ea3e439ef0b66db122584cf455fc83c625ff26
Dinmukhammad Rakhimov <dinmukhammadrakhimov@Gyeongsiks-MacBook-Pro.local>
2019-01-29 14:30:35 +0900

    make it better

--- index.js
+++ index.js
@@ -1,34 +1,11 @@
-const fs = require('fs');
-const express = require('express');
-const { ApolloServer, gql } = require('apollo-server-express');
-const cors = require('cors')
- 
-require('./db/index')
+const express = require('express')
+const app = express()
+const server = require('./src/index')
 
-const typeDefs = gql(fs.readFileSync(__dirname + '/schema.graphql').toString());
+server.applyMiddleware({ app }); // app is from an existing express app
 
-const resolvers = require('./resolvers');
-const context = ({ req }) => {
-  const accessToken = req.headers.authorization || null; //if not signed in the token in serverside is set to null. Anywhere in the server we can cekc this condition if the user is signed in or not
-  return {
-    req,
-    headers: {
-      accessToken: accessToken
-    }
-  }
-};
 
-const server = new ApolloServer({ typeDefs, resolvers, context });   //this line is used when we need headers
-// const server = new ApolloServer({ typeDefs, resolvers });
+// Routers
+app.use('/', require('./src/index'))
 
-const app = express();
-server.applyMiddleware({ app, path: '/graphql' });
-app.use(cors({credentials: true}));
-app.get('/', (req, res)=>{
-  res.send("HEllo Deen")
-})
-const port = process.env.PORT;
-
-app.listen({port: port}, () =>
-  console.log(`🚀 Server ready at https://teliera.herokuapp.com:${port}/graphql`),
-);
+module.exports = app;
---
+++ src/index.js
@@ -0,0 +1,31 @@
+const fs = require('fs');
+const express = require('express');
+const { ApolloServer, gql } = require('apollo-server-express');
+const cors = require('cors')
+ 
+require('./db/index')
+
+const typeDefs = gql(fs.readFileSync(__dirname + '/schema.graphql').toString());
+
+const resolvers = require('./resolvers');
+const context = ({ req }) => {
+  const accessToken = req.headers.authorization || null; //if not signed in the token in serverside is set to null. Anywhere in the server we can cekc this condition if the user is signed in or not
+  return {
+    req,
+    headers: {
+      accessToken: accessToken
+    }
+  }
+};
+
+const server = new ApolloServer({ typeDefs, resolvers, context });   //this line is used when we need headers
+// const server = new ApolloServer({ typeDefs, resolvers });
+
+const app = express();
+server.applyMiddleware({ app, path: '/graphql' });
+app.use(cors({credentials: true}));
+const port = process.env.PORT;
+
+app.listen({port: port}, () =>
+  console.log(`🚀 Server ready at https://teliera.herokuapp.com:${port}/graphql`),
+);

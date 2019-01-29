92d6acec26f04df1d50310220ae9ab0b62ce8e68
Dinmukhammad Rakhimov <dinmukhammadrakhimov@Gyeongsiks-MacBook-Pro.local>
2019-01-29 13:59:52 +0900

    make it better

--- index.js
+++ index.js
@@ -25,8 +25,8 @@ const app = express();
 server.applyMiddleware({ app, path: '/graphql' });
 app.use(cors({credentials: true}));
 
-// const port = process.env.PORT;
+const port = process.env.PORT;
 
-app.listen({ port: process.env.PORT || 4000 }, ({ url }) => {
-  console.log(`🚀 Server ready at ${url}`);
-});
+app.listen({port: port}, () =>
+  console.log(`🚀 Server ready at https://teliera.herokuapp.com:${port}/graphql`),
+);

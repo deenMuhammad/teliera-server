58c7b5d161b6a5f47ed60f9f7ff10b29e12f9deb
Dinmukhammad Rakhimov <dinmukhammadrakhimov@Gyeongsiks-MacBook-Pro.local>
2019-01-29 14:49:40 +0900

    make it better

--- index.js
+++ index.js
@@ -1,10 +1,31 @@
-const express = require('express')
-const app = express()
-const server = require('./src/index')
+const fs = require('fs');
+const express = require('express');
+const { ApolloServer, gql } = require('apollo-server-express');
+const cors = require('cors')
+ 
+require('./db/index')
 
+const typeDefs = gql(fs.readFileSync(__dirname + '/schema.graphql').toString());
 
+const resolvers = require('./resolvers');
+const context = ({ req }) => {
+  const accessToken = req.headers.authorization || null; //if not signed in the token in serverside is set to null. Anywhere in the server we can cekc this condition if the user is signed in or not
+  return {
+    req,
+    headers: {
+      accessToken: accessToken
+    }
+  }
+};
 
-// Routers
-app.use('/', require('./src/index'))
+const server = new ApolloServer({ typeDefs, resolvers, context });   //this line is used when we need headers
+// const server = new ApolloServer({ typeDefs, resolvers });
 
-module.exports = app;
+const app = express();
+server.applyMiddleware({ app });
+app.use(cors({credentials: true}));
+const port = process.env.PORT;
+
+app.listen({port: port}, () =>
+  console.log(`🚀 Server ready at https://teliera.herokuapp.com:${port}/graphql`),
+);
